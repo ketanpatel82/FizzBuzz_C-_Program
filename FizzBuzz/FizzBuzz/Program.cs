4fc20acefc648b969ee3d309a04d409cdb9165bb
unknown <OWNER@office.(none)>
2012-06-12 18:52:16 -0400

    Signed-off-by: unknown <OWNER@office.(none)>

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FizzBuzz");
+                else if( i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else if(i % 3 == 0 )
+                    System.Console.WriteLine("Fizz");
+                else
+                    Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
